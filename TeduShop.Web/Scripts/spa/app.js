fff21dc2f23a791e3ea2364c7a7c12f716a785a4
Your Name <you@example.com>
2017-10-08 21:14:36 +0700

    re-structure for SPA

--- TeduShop.Web/Scripts/spa/app.js
+++
@@ -1,7 +0,0 @@
-﻿var myApp = angular.module("myModule", []);
-myApp.controller("myController", myController);
-myApp.$inject = ['$scope'];
-//declare
-function myController($scope) {
-    $scope.message = "This is message from Controller";
-}
